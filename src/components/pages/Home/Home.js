e0cb051923892177f8fadf8882b34a6a93b13f62
ibnumahtab <ibnumahtab@gmail.com>
2021-11-13 13:11:13 +0600

    Appbar and Banner Complete

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -4,25 +4,21 @@ import AppBar from '../../sharedComponents/AppBar/AppBar';
 import AdBanner from '../../singleComponents/AdBanner/AdBanner';
 import Advantages from '../../singleComponents/Advantages/Advantages';
 import Banner from '../../singleComponents/Banner/Banner';
-import BikeShowcase from '../../singleComponents/BikeShowcase/BikeShowcase';
 import ShopSection from '../../singleComponents/ShopSection/ShopSection';
-import SpecialFeatures from '../../singleComponents/SpecialFeatures/SpecialFeatures';
 import TestimonialSection from '../../singleComponents/TestimonialSection/TestimonialSection';
 
 const Home = () => {
-    return (
-        <div>
-            <AppBar></AppBar>
-            <Banner></Banner>
-            <BikeShowcase></BikeShowcase>
-            <SpecialFeatures></SpecialFeatures>
-            <Advantages></Advantages>
-            <ShopSection></ShopSection>
-            <TestimonialSection></TestimonialSection>
-            <AdBanner></AdBanner>
-            <Footer></Footer>
-        </div>
-    );
+	return (
+		<div>
+			<AppBar></AppBar>
+			<Banner></Banner>
+			<Advantages></Advantages>
+			<ShopSection></ShopSection>
+			<TestimonialSection></TestimonialSection>
+			<AdBanner></AdBanner>
+			<Footer></Footer>
+		</div>
+	);
 };
 
 export default Home;
--- src/components/singleComponents/Banner/Banner.js
+++ src/components/singleComponents/Banner/Banner.js
@@ -1,49 +1,43 @@
 import React from 'react';
-import bike from "../../../img/banner.png"
+import { NavLink } from 'react-router-dom';
+import './Banner.css';
+
+// home page banner
 
 const Banner = () => {
-    return (
-        <div>
-            <div className='container mx-auto'>
-                <div className='flex flex-col md:flex-row items-center'>
-                    <div className='w-3/5'>
-                        <div className='text-5xl font-bold'>
-                            <h2>
-                                <span className='text-primary'>Discover</span>{" "}
-                                your
-                            </h2>
-                            <h2>
-                                Right{" "}
-                                <span className='text-primary'>Bikes</span>
-                            </h2>
-                        </div>
-                        <div className='py-8 pr-24 '>
-                            <p className="mb-2">
-                                You can’t be happier, if you won’t ride your
-                                bike. A long, long ride in your bike will clear
-                                your mind. And the cycle can sing on the streets
-                                of a city.
-                            </p>
-                            <p>
-                                Always back your scooter into the curb and sit
-                                where you can see it. The sound of the throttle
-                                makes me fall in love with our bike. King of the
-                                bikes
-                            </p>
-                        </div>
-                        <div>
-                            <button className='bg-primary px-2 py-1 rounded text-white font-semibold'>
-                                Check Our Bikes
-                            </button>
-                        </div>
-                    </div>
-                    <div className='w-2/5 bg-pink-100 rounded-l-full'>
-                        <img className='mx-auto py-8' src={bike} alt='' />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className=' banner-container'>
+			<div className='container m-auto px-2 py-12 md:py-20 lg:py-40 full-width-all'>
+				<div className='slider h-4/5 text-center'>
+					<div className='text-white p-10'>
+						<h1 className='md:text-2xl text-xl font-extrabold'>
+							{' '}
+							<span className='text-yellow-500'>
+								RideBike{' '}
+							</span>{' '}
+							We Keep You Riding!
+						</h1>
+						<br />
+						<h1 className='md:text-6xl text-3xl uppercase'>
+							If the ride is more fly, then you must buy.
+						</h1>
+						<br />
+						<h4 className='text-xl mb-10'>
+							Riding a race bike is an art - a thing that you do
+							because you feel something inside.
+						</h4>
+						<NavLink
+							className='md:py-3 md:px-24 px-8 py-3 border text-white text-lg font-semibold  hover:bg-gray-700 hover:text-white transition duration-500 ease-in-out'
+							to='/shop'
+						>
+							{' '}
+							Explore
+						</NavLink>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
